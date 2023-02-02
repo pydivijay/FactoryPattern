79bacacfadb5516c804094765836035c09fc1c92
pydivijay <vijaykumar519.pydi@gmail.com>
2023-02-02 16:18:23 +0530

    Add project files.

---
+++ FactoryPattern/Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+Product productA = Factory.GetProductType("A");
+productA.ProductType();
+
+Product productB = Factory.GetProductType("B");
+productB.ProductType();
+abstract class Product
+{
+   public abstract void ProductType();
+}
+class ProductA : Product
+{
+    public override void ProductType()
+    {
+        Console.WriteLine("product A");
+    }
+}
+
+class ProductB : Product
+{
+    public override void ProductType()
+    {
+        Console.WriteLine("product B");
+    }
+}
+
+class Factory
+{
+    public static Product GetProductType(string productType)
+    {
+        if(productType == "A")
+        {
+           return new ProductA();
+        }
+        else if(productType == "B")
+        {
+          return  new ProductB();
+        }
+        else
+        {
+            return null;
+        }
+    }
+}
